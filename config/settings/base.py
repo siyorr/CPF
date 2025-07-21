d10da231019ca00949f28755536adbfb79677e72
siyorr <continue75@naver.com>
2025-07-21 15:00:35 +0900

    modify database

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import json
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -20,7 +23,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5wqdptx92s%h%77iw1b277w2=pzodxwqxbpx+0^i09tq#yb!oz'
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+    
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,4 +1,4 @@
-import environ
+# import environ
 from .base import *
 
 ALLOWED_HOSTS = ['10.1.10.74', 'cpf.aitft.kr']
@@ -6,16 +6,16 @@ STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
 
-env = environ.Env()
-environ.Env.read_env(BASE_DIR / '.env')
+# env = environ.Env()
+# environ.Env.read_env(BASE_DIR / '.env')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'ibm_db_django',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': '49000',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'ibm_db_django',
+#         'NAME': env('DB_NAME'),
+#         'USER': env('DB_USER'),
+#         'PASSWORD': env('DB_PASSWORD'),
+#         'HOST': env('DB_HOST'),
+#         'PORT': '49000',
+#     }
+# }
